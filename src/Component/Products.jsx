a5cf4e0d1203f7fe6668b13aaab3efba510bc958
iam-avinash <avpatil786@gmail.com>
2024-08-27 20:40:24 +0530

    Axios Added - API Intregated

---
+++ src/Component/Products.jsx
@@ -0,0 +1,32 @@
+import axios, { Axios } from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="productsWrapper">
+      {products.map((prod) => {
+        return (
+          <div className="card" key={products.id}>
+            <img src={prod.image} alt="Loading..." />
+            <h6>{prod.title} </h6>
+            <h5>{prod.price}$</h5>
+            <button className="btn">Add to card</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
